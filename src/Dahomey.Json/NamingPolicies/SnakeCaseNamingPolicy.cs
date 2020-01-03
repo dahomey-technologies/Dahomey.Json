b43118218c9d18519144c6a73f91365b1caf7102
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2020-01-03 15:14:49 +0200

    Implementations for SnakeCaseNamingPolicy and KebabCaseNamingPolicy

---
+++ src/Dahomey.Json/NamingPolicies/SnakeCaseNamingPolicy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Dahomey.Json.NamingPolicies
+{
+    public sealed class SnakeCaseNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name)
+        {
+            return name.ToSeparatedCase('_');
+        }
+    }
+}
